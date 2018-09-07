0b3fe453a47786ba625e0896733f03f31e5d834c
unknown <sandyduaso09@gmail.com>
2018-09-07 10:47:14 +0800

    configure .htaccess

---
+++ 404.php
@@ -0,0 +1,46 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sanaysay</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="assets/icon/icofont/css/icofont.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/color/color-1.min.css" id="color" />
+</head>
+<body>
+
+	<div class="error-404">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="text-uppercase col-xs-12">
+					<h1>404</h1>
+					<h5>Page Not Found</h5>
+					<p>oops! page not found</p>
+					<a href="#" class="btn btn-error btn-lg waves-effect">Back to home page</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="assets/plugins/tether/dist/js/tether.min.js"></script>
+
+	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="assets/plugins/Waves/waves.min.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,87 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sanaysay</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+	<link rel="stylesheet" type="text/css" href="assets/icon/icofont/css/icofont.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/color/color-1.min.css" id="color" />
+</head>
+<body>
+	<section class="login p-fixed d-flex text-center bg-primary common-img-bg">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="login-card card-block">
+						<form class="md-float-material">
+							<div class="text-center">
+								<!-- <img src="assets/images/logo-blue.png" alt="logo"> -->
+								<h3 class="text-center text-primary">
+									Sign in to your account
+								</h3>
+								<div class="md-input-wrapper">
+									<input type="email" class="md-form-control" />
+									<label>Email</label>
+								</div>
+								<div class="md-input-wrapper">
+									<input type="password" class="md-form-control">
+									<label>Password</label>
+								</div>
+								<div class="row">
+									<div class="col-sm-6 col-xs-12">
+										<div class="rkmd-checkbox checkbox-rotate checkbox-ripple m-b-25">
+											<label class="input-checkbox checkbox-primary">
+												<input type="checkbox" id="checkbox">
+												<span class="checkbox"></span>
+											</label>
+											<div class="captions">Remember Me</div>
+										</div>
+									</div>
+									<div class="col-sm-6 col-xs-12 forgot-phone text-right">
+										<a href="#" class="text-right f-w-600"> Forget Password ?</a>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-xs-10 offset-xs-1">
+										<button type="button" class="btn btn-primary btn-md btn-block waves-effect text center m-b-20">Login</button>
+									</div>
+								</div>
+								<div class="col-sm-12 col-xs-12 text-center">
+									<span class="text-muted">Don't have an account ?</span>
+									<a href="#" class="f-w-600 p-l-5">Sign up Now</a>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+	<script src="assets/plugins/tether/dist/js/tether.min.js"></script>
+
+	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="assets/plugins/Waves/waves.min.js"></script>
+
+	<script type="text/javascript" src="assets/pages/elements.js"></script>
+</body>
+</html>
